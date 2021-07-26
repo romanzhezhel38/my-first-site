ef853bc9e52a5e5c3453af957fbb6ca76c217c9b
Roman Zhezhel <romanzhezhel38>
2021-07-26 15:26:31 +0300

    add homework 4

--- index.php
+++ index.php
@@ -1,23 +1,36 @@
-<!doctype html>
-<html lang="en">
+﻿<!DOCTYPE html>
+<html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <title>Prof-IT обучение основам</title>
-    <link rel="stylesheet" href="/style.css" type="text/css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Мой первый сайт</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <nav class="nav">
-        <ul>
-            <li><a href="/about.html">Обо мне</a></li>
-            <li><a href="/skills.html">Навыки</a></li>
-        </ul>
-    </nav>
-    <p>
-      курс "Мой первый сайт" на обучающей платформе PROFIT.
-    </p>
-    <p>
-        В итоге должен получиться небольшой сайт.
-    </p>
-    <img src="img/profit.jpg" alt="Здесь находится картинка PROFIT">
+    <div class="container">
+        <div class="row">
+            <div class="panel">
+                <nav class="col-lg-12">
+                    <ul class="nav nav-pills">
+                        <li><a href="/index.html" class="glyphicon glyphicon-home"></a></li>
+                        <li><a href="/news.html">Новости</a></li>
+                        <li><a href="/gallery.html">Фотогалерея</a></li>
+                        <li><a href="/guestbook.html">Гостевая</a></li>
+                        <li><a href="/about.html">Обо мне</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <h3>Главная страница.</h3>
+            <img src="/img/profit.jpg" class="img-responsive" alt="profit">
+        </div>
+    </div>
 </body>
 </html>
